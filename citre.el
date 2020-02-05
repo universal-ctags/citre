7bc8416c1831e8bc8a86fa32f27cb5a0eeb708f2
AmaiKinono <amaikinono@gmail.com>
2020-02-05 18:24:25 +0800

    add: project skeleton

---
+++ citre.el
@@ -0,0 +1,43 @@
+;;; citre.el --- Ctags IDE on the true editor -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Hao Wang
+
+;; Author: Hao Wang <amaikinono@gmail.com>
+;; Maintainer: Hao Wang <amaikinono@gmail.com>
+;; Created: 05 Feb 2020
+;; Keywords: convenience, tools
+;; Homepage: https://github.com/AmaiKinono/ctags-ide
+;; Version: 0
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+;; To see the outline of this file, run M-x outline-minor-mode and
+;; then press C-c @ C-t. To also show the top-level functions and
+;; variable declarations in each section, run M-x occur with the
+;; following query: ^;;;;* \|^(
+
+(provide 'citre)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; citre.el ends here
