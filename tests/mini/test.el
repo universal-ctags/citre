529a221dfa173f4f63fffe0ab6b6bd6404b018b4
Masatake YAMATO <yamato@redhat.com>
2020-11-06 01:57:09 +0900

    tests: update test cases for syncing with the latest library code
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- tests/mini/test.el
+++ tests/mini/test.el
@@ -1,13 +1,22 @@
 (ert-deftest test--get-lines ()
-  "Testing `citre--get-lines'."
+  "Testing `citre-readtags--get-lines'."
   (should (equal
-           (citre--get-lines "z" 'exact default-tags)
+           (citre-readtags--get-lines default-tags "z" 'exact)
            '("z	src/input.h	/^  float z;$/;\"	kind:member	line:18"
              "z	src/input.h	/^  int x, y, z;$/;\"	kind:member	line:9"))))
 
 (ert-deftest test-get-records ()
-  "Testing `citre--get-records'."
-  (should (equal
-           (citre-get-records "z" 'exact default-tags default-directory)
-           `(("z" "member" nil ,(expand-test-file "src/input.h") 18)
-             ("z" "member" nil ,(expand-test-file "src/input.h") 9)))))
+  "Testing `citre-get-records'."
+  (mapc (lambda (record)
+	  (should (equal (citre-readtags-get-field 'name record) "z"))
+	  (should (equal (citre-readtags-get-field 'kind record) "member"))
+	  (let ((line (citre-readtags-get-field 'line record)))
+	    (should (memq line '(9 18)))
+	    (cond
+	     ((eq line 18)
+	      (should (equal (citre-readtags-get-field 'pattern record) "/^  float z;$/;\""))
+	      )
+	     ((eq line 9))
+	     (should (equal (citre-readtags-get-field 'pattern record) "/^  int x, y, z;$/;\""))
+	     )))
+	(citre-get-records default-tags "z" 'exact :require '(name kind line pattern))))
