76892fbf6aba0b07c911819ad1c8b7c78c27df79
AmaiKinono <amaikinono@gmail.com>
2020-11-15 21:32:09 +0800

    fix: case-fold search

--- citre-readtags.el
+++ citre-readtags.el
@@ -1373,7 +1373,7 @@ the position of a tag."
            ;; name and search.  From now on we also use case-fold search to
            ;; deal with projects that uses a case-insensitive language and
            ;; don't have a consistant style on it.
-           (when-let ((bound (when (let ((case-fold-search t))
+           (when-let ((bound (when (let ((case-fold-search nil))
                                      (string-match (regexp-quote name) str))
                                (match-end 0)))
                       (str (substring str 0 bound)))
