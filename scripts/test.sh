79b723591f16d4b16d9921f922ad2f568ab9ea06
AmaiKinono <amaikinono@gmail.com>
2020-11-19 23:23:02 +0800

    test: prettier log

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,24 +7,29 @@
 EMACS=${EMACS:=emacs}
 PRELOAD="citre-readtags-tables.el citre-readtags.el citre.el"
 
-ERROR_FACE="\e[1;31m"
 PASSED_FACE="\e[1;32m"
-NORMAL_FACE='\e[0m'
+ERROR_FACE="\e[1;31m"
+NORMAL_FACE="\e[0m"
 
 d=$(pwd)
 preload_options=
 
+info()
+{
+    printf '[test] %s\n' "$@"
+}
+
 pass()
 {
     printf $PASSED_FACE
-    printf '%s' "$@"
+    printf '[test] %s' "$@"
     printf "$NORMAL_FACE\n"
 }
 
 error()
 {
     printf $ERROR_FACE 1>&2
-    printf '%s' "$@" 1>&2
+    printf '[test] %s' "$@" 1>&2
     printf "$NORMAL_FACE\n" 1>&2
     exit 1
 }
@@ -40,8 +45,9 @@ done
 
 for t in tests/*/test.el; do
     d=$(dirname $t)
+    info "$d"
     (cd $d
-     if ! $EMACS -Q --batch -l ert $preload_options -l ../common.el \ \
+     if ! $EMACS -Q --batch -l ert $preload_options -l ../common.el \
           -l ./test.el -f ert-run-tests-batch-and-exit; then
          error "A test in $d failed."
      fi
