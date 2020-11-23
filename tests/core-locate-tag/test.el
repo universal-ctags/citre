16f854296425325ad8104865b958e4642a7979a4
AmaiKinono <amaikinono@gmail.com>
2020-11-23 20:49:44 +0800

    refactor: rename symbols, readtags -> core
    I prefer shorter names, plus it reflects the logical structure of the
    Citre project.

--- tests/core-locate-tag/test.el
+++ tests/core-locate-tag/test.el
@@ -1,11 +1,11 @@
 (ert-deftest test-locate-tag-by-searching ()
-  "Test `citre-readtags-locate-tag'.
+  "Test `citre-core-locate-tag'.
 This is for situations where the search pattern is presented."
   (dolist (tags '("forward-pattern-tags"
                   "backward-pattern-tags"
                   "combined-backward-pattern-tags"
                   "backward-pattern-with-line-tags"))
-    (let* ((records (citre-readtags-get-records
+    (let* ((records (citre-core-get-records
                      (expand-test-file tags)
                      "temp" 'exact nil
                      :require '(name pattern)
@@ -14,37 +14,37 @@ This is for situations where the search pattern is presented."
       (should (eq (length records) 1))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/add-line.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 5))
+        (should (eq (citre-core-locate-tag record 'use-linum) 5))
         (should (equal (match-string 0)
                        "  real :: pressure(10,10), temp(")))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-indentation.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 5))
+        (should (eq (citre-core-locate-tag record 'use-linum) 5))
         (should (equal (match-string 0)
                        "    real :: pressure(10,10), temp(")))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-matched-str.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 4))
+        (should (eq (citre-core-locate-tag record 'use-linum) 4))
         (should (equal (match-string 0)
                        "  real :: pressure(10,10), temp")))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-definition.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 4))
+        (should (eq (citre-core-locate-tag record 'use-linum) 4))
         (should (equal (match-string 0)
                        "TEMP")))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/remove-all.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum)
+        (should (eq (citre-core-locate-tag record 'use-linum)
                     (if (member tags '("forward-pattern-tags"
                                        "backward-pattern-tags"))
                         1 2)))))))
 
 (ert-deftest test-locate-tag-by-linum ()
-  "Test `citre-readtags-locate-tag'.
+  "Test `citre-core-locate-tag'.
 This is for situations where the search pattern is not provided."
   (dolist (tags '("number-pattern-tags"
                   "number-pattern-with-line-tags"))
-    (let* ((records (citre-readtags-get-records
+    (let* ((records (citre-core-get-records
                      (expand-test-file tags)
                      "temp" 'exact nil
                      :require '(name pattern)
@@ -53,16 +53,16 @@ This is for situations where the search pattern is not provided."
       (should (eq (length records) 1))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/add-line.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 3)))
+        (should (eq (citre-core-locate-tag record 'use-linum) 3)))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-indentation.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 3)))
+        (should (eq (citre-core-locate-tag record 'use-linum) 3)))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-matched-str.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 3)))
+        (should (eq (citre-core-locate-tag record 'use-linum) 3)))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/change-definition.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 3)))
+        (should (eq (citre-core-locate-tag record 'use-linum) 3)))
       (with-current-buffer (find-file-noselect
                             (expand-test-file "src/remove-all.f90"))
-        (should (eq (citre-readtags-locate-tag record 'use-linum) 2))))))
+        (should (eq (citre-core-locate-tag record 'use-linum) 2))))))
