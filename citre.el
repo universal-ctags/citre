ed26e100d58811b5e6f4bffd3f3490fd5725e9f8
AmaiKinono <amaikinono@gmail.com>
2021-06-27 18:44:51 +0800

    doc, enh: rewrite Commentary section in citre.el
    This section is shown as the package description in MELPA. Rewrite it to
    better advertise Citre.

--- citre.el
+++ citre.el
@@ -27,20 +27,17 @@
 
 ;;; Commentary:
 
-;; Citre is an advanced Ctags (or actually, readtags) frontend for Emacs.
+;; Citre is an advanced Ctags (or actually, readtags) frontend for Emacs.  It
+;; offers:
 
-;; To use Citre, you need the readtags program provided by Universal Ctags
-;; (https://github.com/universal-ctags/ctags).  Once you've created a tags file
-;; in your project root, you can use the below commands in your project files:
+;; - auto-completion (by `completion-at-point').
+;; - xref and imenu integration.
+;; - `citre-jump', a `completing-read' UI for jumping to definition.
+;; - `citre-peek', a powerful code reading tool that lets you go down the
+;;   rabbit hole without leaving current buffer.
 
-;; - `citre-mode': Enable `completion-at-point', xref and imenu integration.
-;; - `citre-jump': Jump to the definition of the symbol at point.
-;; - `citre-peek': Peek the definition of the symbol at point.  It's more then
-;;   this.  It's a powerful tool that lets you go down the rabbit hole without
-;;   leaving current buffer.
-
-;; Be sure to read README.md to know more about Citre.  It also points you to a
-;; detailed user manual.  If you haven't received such a file, please visit
+;; Read README.md to know more about Citre.  It also points you to a detailed
+;; user manual.  If you haven't received such a file, please visit
 ;; https://github.com/universal-ctags/citre.
 
 ;;; Code:
