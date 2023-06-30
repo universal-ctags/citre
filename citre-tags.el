b2d64093c548d7fe2d74fe3c8903f97ca13267bd
AmaiKinono <amaikinono@gmail.com>
2023-06-30 23:32:06 +0800

    imenu, fix: empty result from temporary tags files
    Close #153.

--- citre-tags.el
+++ citre-tags.el
@@ -610,18 +610,19 @@ If the ctags program is not found, this returns nil."
       ;; WORKAROUND: If we don't sit for a while, the readtags process will
       ;; freeze.  TOOD: Fix this when uctags offers "edittags" command.
       (sit-for 0.001)
-      (citre-tags-get-tags
-       tags-file nil nil
-       :filter
-       `(not (or ,(citre-readtags-filter
-                   'extras
-                   '("anonymous" "inputFile")
-                   'csv-contain)
-                 ,(citre-readtags-filter-kind "file")))
-       :sorter (citre-readtags-sorter 'line)
-       :require '(name pattern)
-       :optional '(ext-kind-full line typeref scope extras))
-      (delete-file tags-file))))
+      (let ((tags (citre-tags-get-tags
+                   tags-file nil nil
+                   :filter
+                   `(not (or ,(citre-readtags-filter
+                               'extras
+                               '("anonymous" "inputFile")
+                               'csv-contain)
+                             ,(citre-readtags-filter-kind "file")))
+                   :sorter (citre-readtags-sorter 'line)
+                   :require '(name pattern)
+                   :optional '(ext-kind-full line typeref scope extras))))
+        (delete-file tags-file)
+        tags))))
 
 (defun citre-tags-get-tags-in-buffer ()
   "Get tags in buffer."
